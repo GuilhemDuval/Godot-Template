9b83a1a0637d9232cff85d228a6eb58f9aa684f0
Guilhem Duval <guilhem.duval@live.fr>
2026-03-16 23:02:30 +0100

    Added AppConfig class

--- autoload/app_config.gd
+++ autoload/app_config.gd
@@ -1,11 +1,6 @@
-extends Node
+extends RefCounted
+class_name AppConfig
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+const DEFAULT_LEVEL_ID := "test_level"
+const SAVE_DIR := "user://saves/"
+const PREFERENCES_PATH := "user://preferences.cfg"
